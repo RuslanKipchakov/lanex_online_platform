a67c129b83087f6889f665cbabe02acfbc970dad
RuslanKipchakov <your_email@example.com>
2025-09-25 15:39:28 +0500

    Fixed keyboards: set railway domain.

--- telegram/keyboards.py
+++ telegram/keyboards.py
@@ -1,10 +1,12 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
+BASE_URL = "https://lanexonlineplatform-production.up.railway.app"
+
 # Главное меню
 main_menu = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(
         text="Оставить заявку на обучение",
-        web_app=WebAppInfo(url="http://127.0.0.1:8000/html_pages/application_page/application_page.html")
+        web_app=WebAppInfo(url=f"{BASE_URL}/html_pages/application_page/application_page.html")
     )],
     [InlineKeyboardButton(text="Проверить свой уровень", callback_data="check_level")]
 ])
@@ -13,22 +15,22 @@ main_menu = InlineKeyboardMarkup(inline_keyboard=[
 levels_menu = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(
         text="Starter",
-        web_app=WebAppInfo(url="http://127.0.0.1:8000/html_pages/test_pages/levels/starter/starter_page.html")
+        web_app=WebAppInfo(url=f"{BASE_URL}/html_pages/test_pages/levels/starter/starter_page.html")
     )],
     [InlineKeyboardButton(
         text="Elementary",
-        web_app=WebAppInfo(url="http://127.0.0.1:8000/html_pages/test_pages/levels/elementary/elementary_page.html")
+        web_app=WebAppInfo(url=f"{BASE_URL}/html_pages/test_pages/levels/elementary/elementary_page.html")
     )],
     [InlineKeyboardButton(
         text="Pre-Intermediate",
-        web_app=WebAppInfo(url="http://127.0.0.1:8000/html_pages/test_pages/levels/pre_intermediate/pre_intermediate_page.html")
+        web_app=WebAppInfo(url=f"{BASE_URL}/html_pages/test_pages/levels/pre_intermediate/pre_intermediate_page.html")
     )],
     [InlineKeyboardButton(
         text="Intermediate",
-        web_app=WebAppInfo(url="http://127.0.0.1:8000/html_pages/test_pages/levels/intermediate/intermediate_page.html")
+        web_app=WebAppInfo(url=f"{BASE_URL}/html_pages/test_pages/levels/intermediate/intermediate_page.html")
     )],
     [InlineKeyboardButton(
         text="Upper-Intermediate",
-        web_app=WebAppInfo(url="http://127.0.0.1:8000/html_pages/test_pages/levels/upper_intermediate/upper_intermediate_page.html")
+        web_app=WebAppInfo(url=f"{BASE_URL}/html_pages/test_pages/levels/upper_intermediate/upper_intermediate_page.html")
     )],
 ])
