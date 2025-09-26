940500bbd416e43b4d3a5f2bda45ea273682e121
RuslanKipchakov <your_email@example.com>
2025-09-26 21:35:56 +0500

    Installed poetry

--- server.py
+++ server.py
@@ -1,40 +1,30 @@
-# from pathlib import Path
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from fastapi.staticfiles import StaticFiles
-#
-# from api import check_api, application_api
-#
-# BASE_DIR = Path(__file__).resolve().parent
-# app = FastAPI()
-#
-# # Статические страницы
-# app.mount(
-#     "/html_pages",
-#     StaticFiles(directory=BASE_DIR / "html_pages"),
-#     name="html_pages",
-# )
-#
-# # Подключаем роуты
-# app.include_router(check_api.router)
-# app.include_router(application_api.router)  # 👈 добавили
-#
-# # CORS
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
+from pathlib import Path
 from fastapi import FastAPI
-import logging
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
-logging.basicConfig(level=logging.INFO)
+from api import check_api, application_api
+
+BASE_DIR = Path(__file__).resolve().parent
 app = FastAPI()
 
-@app.get("/ping")
-async def ping():
-    logging.info("🔥 /ping endpoint called")
-    return {"status": "ok", "message": "Server is running!"}
+# Статические страницы
+app.mount(
+    "/html_pages",
+    StaticFiles(directory=BASE_DIR / "html_pages"),
+    name="html_pages",
+)
+
+# Подключаем роуты
+app.include_router(check_api.router)
+app.include_router(application_api.router)  # 👈 добавили
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
